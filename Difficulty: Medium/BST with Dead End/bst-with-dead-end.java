a0d533cd0867952027e779b3548f68ee7f70d89c
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-06-09 19:00:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/BST with Dead End/bst-with-dead-end.java
@@ -0,0 +1,11 @@
+class Solution {
+    boolean dfs(Node root, int l, int r) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && l == r) return true;
+        return dfs(root.left, l, root.data - 1) || dfs(root.right, root.data + 1, r);
+    }
+    
+    public boolean isDeadEnd(Node root) {
+        return dfs(root, 1, Integer.MAX_VALUE);
+    }
+}
