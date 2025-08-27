3a9b5b893476944698fd32ce5073a41e100c2f26
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-08-27 22:00:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -1,41 +1,16 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().countTriangles(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to count the number of possible triangles.
-    static int countTriangles(int arr[]) {
-        // code here
-        Arrays.sort(arr);
-        int count = 0, n = arr.length;
-        for (int i = n - 1; i >= 2; --i) {
-            int l = 0, r = i - 1;
-            while (l < r) {
-                if (arr[l] + arr[r] > arr[i]) count += (r-- - l);
-                else ++l;
+    public int countTriangles(int[] a) {
+        Arrays.sort(a);
+        int c = 0, n = a.length;
+        for (int k = n - 1; k >= 2; --k) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (a[i] + a[j] > a[k]) {
+                    c += j - i;
+                    --j;
+                } else ++i;
             }
         }
-        return count;
+        return c;
     }
 }
