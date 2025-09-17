e7080d4170ee3390a45857bf2eaad64f20253448
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-09-17 22:23:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.java
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -1,43 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine());
-        while (t-- > 0) {
-            String s = in.readLine();
-
-            Solution ob = new Solution();
-            out.println(ob.decodeString(s));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
 class Solution {
     static String decodeString(String s) {
-        Stack<String> str = new Stack<>();
-        Stack<Integer> num = new Stack<>();
-        String cur = "";
-        int n = 0;
-
+        Stack<StringBuilder> stStr = new Stack<>();
+        Stack<Integer> stNum = new Stack<>();
+        StringBuilder curr = new StringBuilder();
+        int num = 0;
+        
         for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) n = n * 10 + (c - '0');
-            else if (c == '[') { str.push(cur); num.push(n); cur = ""; n = 0; }
-            else if (c == ']') {
-                String temp = cur;
-                cur = str.pop();
-                cur += temp.repeat(num.pop());
-            } else cur += c;
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '[') {
+                stStr.push(curr);
+                stNum.push(num);
+                curr = new StringBuilder();
+                num = 0;
+            } else if (c == ']') {
+                StringBuilder temp = curr;
+                curr = stStr.pop();
+                int cnt = stNum.pop();
+                while (cnt-- > 0) curr.append(temp);
+            } else {
+                curr.append(c);
+            }
         }
-        return cur;
+        return curr.toString();
     }
 }
-
-
