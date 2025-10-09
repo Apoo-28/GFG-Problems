1f052e7291e4b1c4b4996c88bf541c2c718fe253
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-10-09 20:32:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Postorder Traversal/postorder-traversal.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ArrayList<Integer> postOrder(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Node curr = root;
+        while (curr != null) {
+            if (curr.right == null) {
+                res.add(curr.data);
+                curr = curr.left;
+            } else {
+                Node pred = curr.right;
+                while (pred.left != null && pred.left != curr) pred = pred.left;
+                if (pred.left == null) {
+                    res.add(curr.data);
+                    pred.left = curr;
+                    curr = curr.right;
+                } else {
+                    pred.left = null;
+                    curr = curr.left;
+                }
+            }
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
