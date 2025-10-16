643e44d497416ae5861e61f75b16f87fa130b026
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-10-16 21:50:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove BST keys outside given range/remove-bst-keys-outside-given-range.java
@@ -0,0 +1,24 @@
+class Solution {
+    Node removekeys(Node root, int l, int r) {
+        // code here
+        return dfs(root, l ,r);
+    }
+    Node dfs( Node root, int l, int r){
+        if(root==null)
+        return null;
+        
+        
+        root.left = dfs(root.left, l,r);
+        root.right = dfs(root.right, l,r);
+        
+        //
+        if(root.data<l){
+            return root.right;
+        }
+        if(root.data>r){
+             return root.left;
+        }
+        return root;
+        
+    }
+}
