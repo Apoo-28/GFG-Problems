721b3782729ba8745f050406d162fabbdf137237
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-10-23 23:24:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.java
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.java
@@ -1,48 +1,9 @@
-//{ Driver Code Starts
-import java.util.*;
-
-
-// } Driver Code Ends
-
 class Solution {
-    public int[][] kClosest(int[][] p, int k) {
-        Arrays.sort(p, Comparator.comparingInt(a -> a[0] * a[0] + a[1] * a[1]));
-        return Arrays.copyOfRange(p, 0, k);
+    public ArrayList<ArrayList<Integer>> kClosest(int[][] points, int k) {
+        Arrays.sort(points, (a, b) -> (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]));
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < k; i++)
+            res.add(new ArrayList<>(Arrays.asList(points[i][0], points[i][1])));
+        return res;
     }
 }
-
-//{ Driver Code Starts.
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int t = scanner.nextInt();
-
-        while (t-- > 0) {
-            int k = scanner.nextInt();
-            int n = scanner.nextInt();
-            int[][] points = new int[n][2];
-            for (int i = 0; i < n; i++) {
-                points[i][0] = scanner.nextInt();
-                points[i][1] = scanner.nextInt();
-            }
-            Solution solution = new Solution();
-            int[][] ans = solution.kClosest(points, k);
-
-            Arrays.sort(ans, (a, b) -> {
-                if (a[0] != b[0]) {
-                    return Integer.compare(a[0], b[0]);
-                }
-                return Integer.compare(a[1], b[1]);
-            });
-            for (int[] point : ans) {
-                System.out.println(point[0] + " " + point[1]);
-            }
-            System.out.println("~");
-        }
-
-        scanner.close();
-    }
-}
-// } Driver Code Ends
