f7cf6495c9888bccdb917e87a737281189782a01
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-11-10 17:02:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock Buy and Sell with Cooldown/stock-buy-and-sell-with-cooldown.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+class Solution {
+  maxProfit(arr) {
+    if (arr.length <= 1) return 0;
+
+    let hold = -arr[0];
+    let sold = 0;
+    let rest = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+      const prevHold = hold;
+      const prevSold = sold;
+      const prevRest = rest;
+
+      hold = Math.max(prevHold, prevRest - arr[i]);
+
+      sold = prevHold + arr[i];
+
+      rest = Math.max(prevRest, prevSold);
+    }
+
+    return Math.max(sold, rest);
+  }
+}
