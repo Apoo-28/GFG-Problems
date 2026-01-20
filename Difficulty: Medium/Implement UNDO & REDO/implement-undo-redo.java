30890b7c8eb1284c961bc5820ad7a0770431af78
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2026-01-20 18:55:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement UNDO & REDO/implement-undo-redo.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String doc = "";
+    Stack<Character> redost = new Stack<>();
+    public void append(char X) {
+        doc += X;
+        while (!redost.isEmpty()) redost.pop();
+    }
+
+    public void undo() {
+        if (!doc.isEmpty()) {
+            char last = doc.charAt(doc.length() - 1);
+            doc = doc.substring(0, doc.length() - 1);
+            redost.push(last);
+        }
+    }
+
+    public void redo() {
+        if (!redost.isEmpty()) {
+            char ch = redost.pop();
+            doc += ch;
+        }
+    }
+
+    public String read() { return doc; }
+}
