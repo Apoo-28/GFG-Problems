be5067278cea199101c92830df954a233c9c3b31
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2026-02-02 19:48:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
@@ -1,24 +1,22 @@
 class Solution {
     public int maxCircularSum(int arr[]) {
-        int maxKadane = kadane(arr);
-        int totalSum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        int currMax = arr[0], maxSum = arr[0];
+        int currMin = arr[0], minSum = arr[0];
+        int totalSum = arr[0];
+        int n = arr.length;
+        for(int i=1; i<n; i++) {
+            currMax = Math.max(arr[i], currMax + arr[i]);
+            maxSum = Math.max(maxSum, currMax);
+            
+            currMin = Math.min(arr[i],currMin + arr[i]);
+            minSum = Math.min(minSum, currMin);
+            
             totalSum += arr[i];
-            arr[i] = -arr[i];
         }
-        int maxCircular = totalSum + kadane(arr);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = -arr[i];
-        }
-        return maxCircular == 0 ? maxKadane : Math.max(maxKadane, maxCircular);
-    }
-    
-    private int kadane(int[] arr) {
-        int maxSum = arr[0], currSum = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            currSum = Math.max(arr[i], currSum + arr[i]);
-            maxSum = Math.max(maxSum, currSum);
-        }
-        return maxSum;
+        
+        if(totalSum == minSum)
+            return maxSum;
+            
+        return Math.max(maxSum, totalSum - minSum);
     }
 }
