9343ef596ac77e1efa5f2fcbd84509610f33f1f5
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2026-02-22 23:11:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -1,42 +1,17 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-
-        while (tc-- > 0) {
-            String s[] = br.readLine().split(" ");
-            int arr[] = new int[s.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = Integer.parseInt(s[i]);
-            }
-            int k = Integer.parseInt(br.readLine());
-
-            Solution obj = new Solution();
-            System.out.println(obj.subarrayXor(arr, k));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     public long subarrayXor(int arr[], int k) {
-        // code here
-         long res = 0, prefXOR = 0;
-        Map<Long, Integer> mp = new HashMap<>();
-        mp.put(0L, 1);  
-        for (int val : arr) {
-            prefXOR ^= val; 
-            res += mp.getOrDefault(prefXOR ^ k, 0); 
-            mp.put(prefXOR, mp.getOrDefault(prefXOR, 0) + 1); 
+        int n = arr.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int xor = 0;
+        int ans = 0;
+        map.put(0,1);
+        for(int i=0;i<n;i++){
+            xor ^= arr[i];
+            if(map.containsKey(xor^k)){
+                ans += map.get(xor^k);
+            }
+            map.put(xor, map.getOrDefault(xor, 0) + 1);
         }
-        
-        return res;
+        return ans;
     }
 }
